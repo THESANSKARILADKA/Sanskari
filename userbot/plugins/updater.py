20451f4386fea944868a451ef60c5274b675f1a8
The Sanskari Ladka <69839639+THESANSKARILADKA@users.noreply.github.com>
2020-09-11 12:23:56 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -60,7 +60,7 @@ async def upstream(ups):
     except InvalidGitRepositoryError as error:
         if conf != "now":
             await ups.edit(f"Hey, did you type update🤔.....Okie..My dear sweet master..🤗\
-            \nPlease do type |.update now| to update your DARK-COBRA😎.")
+            \nPlease do type |.update now| to update your SANSKARI USERBOT😎.")
             return
         repo = Repo.init()
         origin = repo.create_remote('upstream', off_repo)
