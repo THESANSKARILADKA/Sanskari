6d8cfaa9a542bae58d04a180d697406cd4ae314f
The Sanskari Ladka <69839639+THESANSKARILADKA@users.noreply.github.com>
2020-09-13 09:40:30 +0530

    Update telegraph.py

--- userbot/plugins/telegraph.py
+++ userbot/plugins/telegraph.py
@@ -1,4 +1,4 @@
-"""@telegraph Utilities Fix by @hellboi_atul
+"""@telegraph Utilities Fix by @thesanskari_ladka
 Available Commands:
 .telegraph media as reply to a media
 .telegraph text as reply to a large text"""
