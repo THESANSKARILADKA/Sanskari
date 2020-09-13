49a2fdc32366118f228bc2d416ec33cd53f7216f
The Sanskari Ladka <69839639+THESANSKARILADKA@users.noreply.github.com>
2020-09-13 13:42:11 +0530

    Add files via upload

---
+++ Tapatap.py
@@ -0,0 +1,23 @@
+#Credit To @Kraken_The_BadASS . Keep credit if you are going to edit it.
+
+
+import random, re
+from uniborg.util import admin_cmd
+import asyncio
+from telethon import events
+
+@borg.on(admin_cmd(pattern="tapatap ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+       
+        await event.edit("**Nipple 😋 se Tapak Raha Hai Pasina😥**")
+        await asyncio.sleep(2)
+        await event.edit("**Nipple 🤭 se Tapak Rha h pasina🤤**")
+        await asyncio.sleep(2)
+        await event.edit("**Bhegi💦 hui gand, Aur lathpath Seena🤭**")
+        await asyncio.sleep(2)
+        await event.edit("**Abb Tumhi btao Galib🙄**")
+        await asyncio.sleep(2)
+        await event.edit("Itni garmi me😰 \n **Koi Kaise Thoke Hasina😵**")
+        await asyncio.sleep(3.5)
+        await event.edit("**Nipple Se Tapak Raha Hai Pasina💦 \nNipple Se Tapak Raha Hai Pasina💦 \nBhigi Hui Gaand🤭 Aur Lathpath Seena😐 \nAab Tumhi Batao Ghalib🙄 \nItni Garmi Mein😰 Koi Kaise Thoke😵 Hasina😋**")
